c44326fe90afbdec56440668483254b643a51465
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-11-23 20:34:19 +0700

    Add NetworkModule

--- app/src/main/java/com/ajailani/projekan/di/NetworkModule.kt
+++ app/src/main/java/com/ajailani/projekan/di/NetworkModule.kt
@@ -1,4 +1,22 @@
 package com.ajailani.projekan.di
 
+import com.ajailani.projekan.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
 }
