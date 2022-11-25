5f06ad9ba0c739d606e6bd5cf5fe65d6beb45041
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-11-25 09:50:14 +0700

    Update WelcomeScreen

--- app/src/main/java/com/ajailani/projekan/ui/feature/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/ajailani/projekan/ui/feature/welcome/WelcomeScreen.kt
@@ -3,6 +3,8 @@ package com.ajailani.projekan.ui.feature.welcome
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,54 +23,58 @@ fun WelcomeScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .verticalScroll(rememberScrollState())
     ) {
-        Text(
-            modifier = Modifier.padding(top = 50.dp),
-            text = stringResource(id = R.string.welcome_to_projekan),
-            color = MaterialTheme.colors.onBackground,
-            style = MaterialTheme.typography.h1
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(
-            text = stringResource(id = R.string.manage_your_projects),
-            color = Grey,
-            style = MaterialTheme.typography.body1
-        )
-        Spacer(modifier = Modifier.height(30.dp))
-        Image(
-            modifier = Modifier.size(250.dp),
-            painter = painterResource(id = R.drawable.clip_art_working),
-            contentDescription = "Working clip art"
-        )
-        Spacer(modifier = Modifier.height(50.dp))
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            shape = MaterialTheme.shapes.large,
-            onClick = onNavigateToLogin
+        Column(
+            modifier = Modifier.padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                modifier = Modifier.padding(5.dp),
-                text = stringResource(id = R.string.login)
+                modifier = Modifier.padding(top = 50.dp),
+                text = stringResource(id = R.string.welcome_to_projekan),
+                color = MaterialTheme.colors.onBackground,
+                style = MaterialTheme.typography.h1
             )
-        }
-        Spacer(modifier = Modifier.height(15.dp))
-        OutlinedButton(
-            modifier = Modifier.fillMaxWidth(),
-            shape = MaterialTheme.shapes.large,
-            colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color.Transparent),
-            border = BorderStroke(
-                width = 1.dp,
-                color = MaterialTheme.colors.primary
-            ),
-            onClick = { /*TODO*/ }
-        ) {
+            Spacer(modifier = Modifier.height(10.dp))
             Text(
-                modifier = Modifier.padding(5.dp),
-                text = stringResource(id = R.string.register),
-                color = MaterialTheme.colors.primary
+                text = stringResource(id = R.string.manage_your_projects),
+                color = Grey,
+                style = MaterialTheme.typography.body1
             )
+            Spacer(modifier = Modifier.height(30.dp))
+            Image(
+                modifier = Modifier.size(250.dp),
+                painter = painterResource(id = R.drawable.clip_art_working),
+                contentDescription = "Working clip art"
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                shape = MaterialTheme.shapes.large,
+                onClick = onNavigateToLogin
+            ) {
+                Text(
+                    modifier = Modifier.padding(5.dp),
+                    text = stringResource(id = R.string.login)
+                )
+            }
+            Spacer(modifier = Modifier.height(15.dp))
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth(),
+                shape = MaterialTheme.shapes.large,
+                colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color.Transparent),
+                border = BorderStroke(
+                    width = 1.dp,
+                    color = MaterialTheme.colors.primary
+                ),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    modifier = Modifier.padding(5.dp),
+                    text = stringResource(id = R.string.register),
+                    color = MaterialTheme.colors.primary
+                )
+            }
         }
     }
 }
