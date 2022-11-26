1deed53504b38101b1814a2c0e4bdf7785d6ac6f
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-11-26 11:50:32 +0700

    Add UserCredentialRepository

---
+++ app/src/main/java/com/ajailani/projekan/domain/repository/UserCredentialRepository.kt
@@ -0,0 +1,11 @@
+package com.ajailani.projekan.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+
+interface UserCredentialRepository {
+    suspend fun saveAccessToken(accessToken: String)
+
+    fun getAccessToken(): Flow<String>
+
+    suspend fun deleteAccessToken()
+}
