f255a186c29589efbb818514d9fa5728af510cb1
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-11-28 12:08:15 +0700

    Add extra large shape

--- app/src/main/java/com/ajailani/projekan/ui/theme/Shape.kt
+++ app/src/main/java/com/ajailani/projekan/ui/theme/Shape.kt
@@ -1,12 +1,18 @@
 package com.ajailani.projekan.ui.theme
 
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
     small = RoundedCornerShape(8.dp),
     medium = RoundedCornerShape(15.dp),
-    large = CircleShape
+    large = RoundedCornerShape(20.dp)
 )
+
+val Shapes.extraLarge: CornerBasedShape
+    @Composable
+    get() = CircleShape
