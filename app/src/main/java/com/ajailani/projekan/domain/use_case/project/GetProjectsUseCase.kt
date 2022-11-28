59c107c0d57d14163910b88751e51c68da34a2ac
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-11-28 18:51:11 +0700

    Add GetProjectsUseCase

---
+++ app/src/main/java/com/ajailani/projekan/domain/use_case/project/GetProjectsUseCase.kt
@@ -0,0 +1,19 @@
+package com.ajailani.projekan.domain.use_case.project
+
+import com.ajailani.projekan.domain.repository.ProjectRepository
+import com.ajailani.projekan.util.ProjectType
+import javax.inject.Inject
+
+class GetProjectsUseCase @Inject constructor(
+    private val projectRepository: ProjectRepository
+) {
+    operator fun invoke(
+        page: Int,
+        size: Int,
+        type: ProjectType
+    ) = projectRepository.getProjects(
+        page = page,
+        size = size,
+        type = type
+    )
+}
