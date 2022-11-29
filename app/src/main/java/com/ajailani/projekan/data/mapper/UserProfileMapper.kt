c0713d0b3da2f9811bedcc43889ff06b9657a6ab
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-11-29 12:09:06 +0700

    Add implementation of UserProfileRepository

---
+++ app/src/main/java/com/ajailani/projekan/data/mapper/UserProfileMapper.kt
@@ -0,0 +1,7 @@
+package com.ajailani.projekan.data.mapper
+
+import com.ajailani.projekan.data.remote.dto.UserProfileDto
+import com.ajailani.projekan.domain.model.UserProfile
+
+fun UserProfileDto.toUserProfile() =
+    UserProfile(name = name)
--- app/src/main/java/com/ajailani/projekan/di/RepositoryModule.kt
+++ app/src/main/java/com/ajailani/projekan/di/RepositoryModule.kt
@@ -3,6 +3,7 @@ package com.ajailani.projekan.di
 import com.ajailani.projekan.data.repository.AuthRepositoryImpl
 import com.ajailani.projekan.data.repository.ProjectRepositoryImpl
 import com.ajailani.projekan.data.repository.UserCredentialRepositoryImpl
+import com.ajailani.projekan.data.repository.UserProfileRepositoryImpl
 import com.ajailani.projekan.domain.repository.AuthRepository
 import com.ajailani.projekan.domain.repository.ProjectRepository
 import com.ajailani.projekan.domain.repository.UserCredentialRepository
@@ -28,4 +29,9 @@ abstract class RepositoryModule {
     abstract fun bindProjectRepository(
         projectRepositoryImpl: ProjectRepositoryImpl
     ): ProjectRepository
+
+    @Binds
+    abstract fun bindUserProfileRepository(
+        userProfileRepositoryImpl: UserProfileRepositoryImpl
+    ): UserCredentialRepository
 }
