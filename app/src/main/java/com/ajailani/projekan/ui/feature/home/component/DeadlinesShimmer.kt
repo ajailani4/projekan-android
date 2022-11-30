030fbc9eaf735782a7028ffd047c06f2e9bf2ef8
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-11-30 10:13:26 +0700

    Update DeadlinesShimmer

--- app/src/main/java/com/ajailani/projekan/ui/feature/home/component/DeadlinesShimmer.kt
+++ app/src/main/java/com/ajailani/projekan/ui/feature/home/component/DeadlinesShimmer.kt
@@ -2,6 +2,7 @@ package com.ajailani.projekan.ui.feature.home.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -17,74 +18,76 @@ import com.valentinilk.shimmer.shimmer
 fun DeadlinesShimmer() {
     val shimmerInstance = rememberShimmer(shimmerBounds = ShimmerBounds.View)
 
-    Row(modifier = Modifier.padding(start = 20.dp)) {
-        for (i in 1..2) {
-            Card(
-                modifier = Modifier.shimmer(shimmerInstance),
-                shape = MaterialTheme.shapes.large,
-                elevation = 0.dp
-            ) {
-                Column(
-                    modifier = Modifier
-                        .width(280.dp)
-                        .padding(15.dp)
+    Row {
+        LazyRow(contentPadding = PaddingValues(horizontal = 20.dp)) {
+            items(2) {
+                Card(
+                    modifier = Modifier.shimmer(shimmerInstance),
+                    shape = MaterialTheme.shapes.large,
+                    elevation = 0.dp
                 ) {
-                    Row {
-                        Box(
-                            modifier = Modifier
-                                .size(50.dp)
-                                .background(color = BackgroundShimmer)
-                                .clip(MaterialTheme.shapes.small)
-                                .shimmer(shimmerInstance)
-                        )
-                        Spacer(modifier = Modifier.width(15.dp))
-                        Column {
+                    Column(
+                        modifier = Modifier
+                            .width(280.dp)
+                            .padding(15.dp)
+                    ) {
+                        Row {
                             Box(
                                 modifier = Modifier
-                                    .size(width = 100.dp, height = 20.dp)
-                                    .background(color = BackgroundShimmer)
                                     .clip(MaterialTheme.shapes.small)
+                                    .size(50.dp)
+                                    .background(color = BackgroundShimmer)
                                     .shimmer(shimmerInstance)
                             )
-                            Spacer(modifier = Modifier.height(5.dp))
-                            Box(
-                                modifier = Modifier
-                                    .size(width = 120.dp, height = 20.dp)
-                                    .background(color = BackgroundShimmer)
-                                    .clip(MaterialTheme.shapes.small)
-                                    .shimmer(shimmerInstance)
-                            )
-                            Spacer(modifier = Modifier.height(30.dp))
-                            Row {
+                            Spacer(modifier = Modifier.width(15.dp))
+                            Column {
                                 Box(
                                     modifier = Modifier
-                                        .size(width = 40.dp, height = 15.dp)
-                                        .background(color = BackgroundShimmer)
                                         .clip(MaterialTheme.shapes.small)
+                                        .size(width = 100.dp, height = 20.dp)
+                                        .background(color = BackgroundShimmer)
                                         .shimmer(shimmerInstance)
                                 )
-                                Spacer(modifier = Modifier.width(10.dp))
+                                Spacer(modifier = Modifier.height(5.dp))
                                 Box(
                                     modifier = Modifier
-                                        .size(width = 40.dp, height = 15.dp)
-                                        .background(color = BackgroundShimmer)
                                         .clip(MaterialTheme.shapes.small)
+                                        .size(width = 120.dp, height = 20.dp)
+                                        .background(color = BackgroundShimmer)
+                                        .shimmer(shimmerInstance)
+                                )
+                                Spacer(modifier = Modifier.height(30.dp))
+                                Row {
+                                    Box(
+                                        modifier = Modifier
+                                            .clip(MaterialTheme.shapes.small)
+                                            .size(width = 40.dp, height = 15.dp)
+                                            .background(color = BackgroundShimmer)
+                                            .shimmer(shimmerInstance)
+                                    )
+                                    Spacer(modifier = Modifier.width(10.dp))
+                                    Box(
+                                        modifier = Modifier
+                                            .clip(MaterialTheme.shapes.small)
+                                            .size(width = 40.dp, height = 15.dp)
+                                            .background(color = BackgroundShimmer)
+                                            .shimmer(shimmerInstance)
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(20.dp))
+                                Box(
+                                    modifier = Modifier
+                                        .clip(MaterialTheme.shapes.small)
+                                        .size(width = 100.dp, height = 20.dp)
+                                        .background(color = BackgroundShimmer)
                                         .shimmer(shimmerInstance)
                                 )
                             }
-                            Spacer(modifier = Modifier.height(20.dp))
-                            Box(
-                                modifier = Modifier
-                                    .size(width = 100.dp, height = 20.dp)
-                                    .background(color = BackgroundShimmer)
-                                    .clip(MaterialTheme.shapes.small)
-                                    .shimmer(shimmerInstance)
-                            )
                         }
                     }
                 }
+                Spacer(modifier = Modifier.width(15.dp))
             }
-            Spacer(modifier = Modifier.width(15.dp))
         } 
     }
 }
