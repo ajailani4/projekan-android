10e9787ad720b412390821860d38a2b36f5dd9c2
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-12-10 16:43:38 +0700

    Add TaskRepository interface

---
+++ app/src/main/java/com/ajailani/projekan/domain/repository/TaskRepository.kt
@@ -0,0 +1,11 @@
+package com.ajailani.projekan.domain.repository
+
+import com.ajailani.projekan.data.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface TaskRepository {
+    fun addTask(
+        projectId: String,
+        title: String
+    ): Flow<Resource<Any>>
+}
